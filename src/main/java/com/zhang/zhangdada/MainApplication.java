5507cd20a87262d9cd1346a9b6b5ef61a11294ef
“ZhangRonn” <2830913880@qq.com>
2024-06-09 19:48:19 +0800

    master

--- src/main/java/com/zhang/zhangdada/MainApplication.java
+++ src/main/java/com/zhang/zhangdada/MainApplication.java
@@ -21,7 +21,7 @@ public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
         System.out.println("启动成功啦啦！！！！！！！！！");
-        System.out.println("启动成功啦啦！！！！！！！！！");
+        System.out.println("启动成功啦啦！！！");
     }
 
 }
