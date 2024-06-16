93da63447b42196451049ba7f66972f965081941
“ZhangRonn” <2830913880@qq.com>
2024-06-16 17:44:29 +0800

    Merge branch 'zhang'
    # Conflicts:
    #	src/main/java/com/zhang/zhangdada/MainApplication.java

--- src/main/java/com/zhang/zhangdada/controller/UserAnswerController.java
+++ src/main/java/com/zhang/zhangdada/controller/UserAnswerController.java
@@ -1,13 +1,18 @@
 package com.zhang.zhangdada.controller;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.json.JSONUtil;
 import com.zhang.zhangdada.common.BaseResponse;
 import com.zhang.zhangdada.common.ErrorCode;
 import com.zhang.zhangdada.common.ResultUtils;
+import com.zhang.zhangdada.exception.BusinessException;
 import com.zhang.zhangdada.exception.ThrowUtils;
 import com.zhang.zhangdada.model.dto.userAnswer.UserAnswerAddRequest;
+import com.zhang.zhangdada.model.entity.App;
 import com.zhang.zhangdada.model.entity.User;
 import com.zhang.zhangdada.model.entity.UserAnswer;
+import com.zhang.zhangdada.model.enums.ReviewStatusEnum;
+import com.zhang.zhangdada.service.AppService;
 import com.zhang.zhangdada.service.UserAnswerService;
 import com.zhang.zhangdada.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController("/userAnswer")
 @Slf4j
@@ -29,22 +35,39 @@ public class UserAnswerController {
     @Resource
     private UserService userService;
 
+    @Resource
+    private AppService appService;
     @PostMapping("/addUserAnswer")
     public BaseResponse<Boolean> addUserAnswer(@RequestBody UserAnswerAddRequest userAnswerAddRequest, HttpServletRequest request) {
         ThrowUtils.throwIf(userAnswerAddRequest == null, ErrorCode.PARAMS_ERROR,"请求参数不能为空");
 
-        User loginUser = userService.getLoginUser(request);
-        ThrowUtils.throwIf(BeanUtil.isEmpty(loginUser),ErrorCode.NO_AUTH_ERROR);
-
         UserAnswer userAnswer = new UserAnswer();
         BeanUtil.copyProperties(userAnswerAddRequest,userAnswer);
-        userAnswerService.validUserAnswer(userAnswer);
+        List<String> choices = userAnswerAddRequest.getChoices();
+        userAnswer.setChoices(JSONUtil.toJsonStr(choices));
 
-        boolean save = userAnswerService.save(userAnswer);
-        ThrowUtils.throwIf(!save,ErrorCode.OPERATION_ERROR,"添加失败");
+//        验证用户回答是否已经存在
+        UserAnswer byId = userAnswerService.getById(userAnswer.getId());
+        ThrowUtils.throwIf(!BeanUtil.isEmpty(byId),ErrorCode.SYSTEM_ERROR,"用户回答已经存在");
+
+        userAnswerService.validUserAnswer(userAnswer);
+        User loginUser = userService.getLoginUser(request);
+        userAnswer.setUserId(loginUser.getId());
+
+        App app = appService.getById(userAnswer.getAppId());
+        if (!ReviewStatusEnum.PASS.equals(ReviewStatusEnum.getEnumByValue(app.getReviewStatus()))){
+            throw new BusinessException(ErrorCode.OPERATION_ERROR, "应用未通过审核，操作失败");
+        }
+
+        try {
+          Boolean  save = userAnswerService.save(userAnswer);
+          ThrowUtils.throwIf(!save,ErrorCode.SYSTEM_ERROR,"添加失败");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+//        todo 调用评分模块
 
         return ResultUtils.success(true);
-
     }
 
 }
